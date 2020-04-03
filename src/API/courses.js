5df21aecdbc1c8c0fe45ec879a8caada27560150
Dũng Võ <vodung0801@gmail.com>
2020-04-04 00:26:31 +0700

    Change Link API

--- src/API/courses.js
+++ src/API/courses.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "http://localhost:3005/api/QuanLyKhoaHoc/"
+    baseURL: "https://courseserver.herokuapp.com/api/QuanLyKhoaHoc/"
 });
 
 export default api;
